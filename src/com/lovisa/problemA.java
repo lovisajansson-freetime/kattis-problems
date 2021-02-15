e1fcd9fa0c25eaad911e12329857c75c72e5a68d
Lovisa Jansson <lovisa.jansson96@gmail.com>
2021-02-15 10:03:30 +0100

    kattis handelsbanken test

--- src/com/lovisa/problemA.java
+++ src/com/lovisa/problemA.java
@@ -2,14 +2,14 @@ package com.lovisa;
 
 import java.util.Scanner;
 
-public class hb {
+public class problemA {
 
-
+//problem a
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         for (int i =1;i<=n;i++){
-            System.out.println(i + " Abracadabra");
+
         }
     }
     }
--- src/com/lovisa/problemC.java
+++ src/com/lovisa/problemC.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 
-public class c2 {
+public class problemC {
 
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
